218a19ff2ce43c9344a38cc60aa20fc3104a275c
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-02-13 20:46:55 +0600

    Start Learn Typescript

---
+++ convert-javascript/script.js
@@ -0,0 +1,6 @@
+"use strict";
+// Welcome To TypeScript!
+// Watching File Changes Detected Commend (tsc -w)
+// Typescript (tsc init, tsc)
+var country = 'A Beautiful CountryBangladesh, Dhaka!';
+console.log(country);
---
+++ root-typescript/script.ts
@@ -0,0 +1,5 @@
+// Welcome To TypeScript!
+// Watching File Changes Detected Commend (tsc -w)
+// Typescript (tsc init, tsc)
+const country = 'A Beautiful CountryBangladesh, Dhaka!'
+console.log(country);
