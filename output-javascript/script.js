38294126905a44c5b48abad85be5261134ec36f5
Sajjad Sadiq <sajjadsadiq2020@gmail.com>
2021-02-13 21:41:20 +0600

    Learn Basics Type

--- output-javascript/script.js
+++ output-javascript/script.js
@@ -1,6 +1,30 @@
 "use strict";
 // Welcome To TypeScript!
 // Watching File Changes Detected Commend (tsc -w)
-// Typescript (tsc init, tsc)
+// Typescript Initialize (tsc init, tsc)
 var country = 'A Beautiful CountryBangladesh, Dhaka!';
-console.log(country);
+//console.log(country);
+//------------Learn Basics Types-----------
+// Function Sum
+function sum(one, two) {
+    return one + two;
+}
+var sumResult = sum(20, 43);
+console.log(sumResult);
+// Array List
+var foods = ['Apple', 'Banana', 'Candy', 'Bran', 'Clove', 'Dijon'];
+foods.push('Door');
+console.log(foods);
+// Mixed Array, Number, String, Boolean, Object All Array.
+var mixedArray = ['Apple', 'Banana', 23, 81, false, true, { name: 'Sajjad Sadiq' }];
+console.log(mixedArray);
+// Object || intelligent || Human Error Small ||
+// jai function ta ba jai object ta age declaration korbo oitai tar syntax dore nibe.
+var friendsInfo = {
+    name: 'Sabbir Hasan',
+    age: 24,
+    school: 'oxford international school',
+    days: true,
+};
+//friendsInfo.country = 'Bangladesh' // Syntax age declaration kore nai
+//--------- Lean Explicit & Union Types -------------
--- root-typescript/script.ts
+++ root-typescript/script.ts
@@ -1,5 +1,50 @@
 // Welcome To TypeScript!
 // Watching File Changes Detected Commend (tsc -w)
-// Typescript (tsc init, tsc)
+// Typescript Initialize (tsc init, tsc)
 const country = 'A Beautiful CountryBangladesh, Dhaka!'
-console.log(country);
+//console.log(country);
+
+
+//------------Learn Basics Types-----------
+// Function Sum
+function sum(one:number, two:number){
+    return one + two;
+}
+const sumResult = sum(20, 43)
+console.log(sumResult);
+
+
+// Array List
+const foods = ['Apple', 'Banana', 'Candy', 'Bran', 'Clove', 'Dijon']
+foods.push('Door')
+console.log(foods);
+
+
+// Mixed Array, Number, String, Boolean, Object All Array.
+const mixedArray = ['Apple', 'Banana', 23, 81, false, true, {name:'Sajjad Sadiq'}]
+console.log(mixedArray);
+
+
+// Object || intelligent || Human Error Small ||
+// jai function ta ba jai object ta age declaration korbo oitai tar syntax dore nibe.
+let friendsInfo = {
+    name: 'Sabbir Hasan',
+    age: 24,
+    school: 'oxford international school',
+    days: true,
+}
+//friendsInfo.country = 'Bangladesh' // Syntax age declaration kore nai
+
+
+
+
+
+
+
+//--------- Lean Explicit & Union Types -------------
+
+
+
+
+
+
